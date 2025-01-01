857ce178ce1c144cde70ca6dc9651b6d986693bd
gyuuuuul <scibsj13@gmail.comcd git_test/>
2025-01-01 19:00:58 +0900

    네이버 뉴스 제목 및 하이퍼링크 추출하기 - 수정 1

--- web_crawlling_practice4.py
+++ web_crawlling_practice4.py
@@ -1,6 +1,6 @@
 # requests와 beautifulsoup를 이용, 추출 방법
-# 크롬 개발자도구에서 커서 모양의 아이콘을 클릭 -> 추출을 원하는 부분을 클릭 -> 해당 부분의 HTML 태그 및 class (ex. a와 href)를 분석하여 select( ) 함수로 추출
-# 네이버 뉴스에서 태그 및 클래스는: "a.news_tit" 임.
+# 크롬 개발자도구에서 커서 모양의 아이콘을 클릭 -> 추출을 원하는 부분을 클릭 -> 해당 부분의 HTML 태그 및 속성을 (ex. a와 href)를 분석하여 select( ) 함수로 추출
+# 네이버 뉴스에서 태그 및 속성(attrs)는: "a.news_tit" 임.
 
 
 # 1. improt로 패키지 불러오기
@@ -26,10 +26,17 @@ html_text = response.text
 
 soup = bs(response.text, 'html.parser')
 
-# 6.뉴스 제목 텍스트 추출
+# 6. "news_titles"라는 변수에 제목 해당되는 html을 지정. 그 후 '.get_text()' 함수를 이용해 제목을 추출한다.
 
 news_titles = soup.select("a.news_tit")
 
 for i in news_titles:
     title = i.get_text()
     print(title)
+
+# 7.뉴스 하이퍼링크 추출. '.atters['href']'함수를 이용. 
+
+for i in news_titles:
+    href = i.attrs['href']
+    print(href)
+
