dfc0d6a0548e4db89a49dbffa9450d6c411608d5
gyuuuuul <scibsj13@gmail.com>
2025-01-03 09:41:04 +0900

    크롬-네이버 반응형 검색. 기사 개수 설정 가능

--- selenium_chrome_p4.py
+++ selenium_chrome_p4.py
@@ -3,14 +3,19 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+import os
+
+# Desktop 경로 설정
+desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+print(desktop)
 
 # 키워드 및 크롤링 설정
 Keyword = input('1. 수집 검색어는 무엇입니까?: ')
 ArticleNum = int(input('2. 몇 건을 수집하시겠습니까?: '))
 
-# 저장 파일 설정
-ft_name = input('3.결과를 저장할 txt형식의 파일명을 쓰세요(예: 파일명.txt): ')
-fx_name = input('4.결과를 저장할 xlsx형식의 파일명을 쓰세요(예: 파일명.xlsx): ')
+# 저장 파일 이름 및 경로 설정
+txt_name = os.path.join(desktop, input('3.결과를 저장할 txt형식의 파일명을 쓰세요(예: 파일명.txt): '))
+excel_name = os.path.join(desktop, input('4.결과를 저장할 xlsx형식의 파일명을 쓰세요(예: 파일명.xlsx): '))
 
 # Selenium 초기 설정
 options = webdriver.ChromeOptions()
@@ -18,7 +23,7 @@ options.add_argument('start-maximized')
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(5) 
 
-# 네이버 뉴스 페이지 열기
+# 네이버 뉴스 페이지 열기. XPATH가 아닌 LINK_TEXT로 했음.
 driver.get('https://www.naver.com/')
 driver.find_element(By.ID, 'query').send_keys(Keyword + '\n')
 driver.find_element(By.LINK_TEXT, '뉴스').click()
@@ -53,7 +58,7 @@ while len(title2) < ArticleNum:
         title2.append(title)
         url2.append(url)
 
-        with open(ft_name, 'a', encoding='utf-8') as f:
+        with open(txt_name, 'a', encoding='utf-8') as f:
             f.write(f"1. 연번: {no}\n2. 기사 제목: {title}\n3. 링크: {url}\n\n")
 
         print(f"1. 연번: {no}\n2. 기사 제목: {title}\n3. 링크: {url}\n")
@@ -67,6 +72,6 @@ print("데이터 수집이 완료되었습니다!")
 
 # Pandas DataFrame 저장
 df = pd.DataFrame({'연번': sn2, '기사 제목': title2, '링크': url2})
-df.to_excel(fx_name, index=False, sheet_name='뉴스 기사')
+df.to_excel(excel_name, index=False, sheet_name='뉴스 기사')
 
 driver.quit()
